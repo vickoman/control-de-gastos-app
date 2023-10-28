297c720932f2a2426f7bc7dd2214aaef7b2306d6
vickoman <vicmandlagasca@gmail.com>
2023-10-27 23:52:07 -0500

    Molecule button component

--- src/components/moleculas/Btnsave.jsx
+++ src/components/moleculas/Btnsave.jsx
@@ -1,9 +1,12 @@
 import styled from "styled-components";
-
-export function Btnsave() {
+import { Icono } from "../../index";
+export function Btnsave({ funcion, titulo, bgcolor, icono}) {
     return (
-        <Container>
-            <h1>Componente</h1>
+        <Container type="submit" bgcolor={bgcolor}>
+            <Icono>{icono}</Icono>
+            <span className="btn" onClick={funcion}>
+                {titulo}
+            </span>
         </Container>
     );
 }
@@ -13,4 +16,28 @@ const Container =styled.button`
     justify-content: center;
     align-items: center;
     text-decoration: none;
+    border: none;
+    gap: 10px;
+    background-color: initial;
+    .btn {
+        background: ${(props) => props.bgcolor};
+        padding: 0.6em 1.3em;
+        font-weight: 900;
+        font-size: 18px;
+        border: 3px solid black;
+        border-radius: 0.4em;
+        box-shadow: 0.1em 0.1em #000;
+        transition: 0.2s;
+        white-space:1px;
+        color: #000;
+        cursor: pointer;
+        &:hover {
+            transform: translate(-0.5em, -0.5em);
+            box-shadow: 0.15em 0.15em #000;
+        }
+        &:active {
+            transform: translate(0.5em, 0.5em);
+            box-shadow: 0.5em 0.5em #000;
+        }
+    }
 `
