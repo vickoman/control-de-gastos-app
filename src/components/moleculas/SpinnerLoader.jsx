7ffde4d80530b733262df6045b829f4326731b84
vickoman <vicmandlagasca@gmail.com>
2023-11-01 11:08:51 -0500

    Create new spinner

---
+++ src/components/moleculas/SpinnerLoader.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { HashLoader } from 'react-spinners';
+import { useOperaciones } from '../../index';
+export function SpinnerLoader() {
+    const { colorCategoria } = useOperaciones();
+    return (
+        <Container>
+            <HashLoader color="#7f3ceb" size={200} />
+        </Container>
+    );
+}
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1000;
+    background-color: rgb(31, 31, 31);
+    transform: all 0.3s;
+`;
