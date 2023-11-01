9c64ddf01aeae3a5cb637af5914a12273fb18b5a
vickoman <vicmandlagasca@gmail.com>
2023-11-01 00:36:54 -0500

    Add Category store

---
+++ src/store/CategoriaStore.jsx
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+import { MostratCategoria,InsertarCategoria, EditarCategoria } from "../index";
+
+export const useCategoriasStore = create((set, get) => ({
+
+    dataCategoria: [],
+    mostrarCategorias: async (p) => {
+        const response = await MostratCategoria(p);
+        set({dataCategoria: response});
+        return response;
+    },
+    insertarCategoria: async(p) => {
+        await InsertarCategoria(p);
+        const {mostrarCategorias} = get();
+        set(mostrarCategorias(p));
+    },
+    eliminarCategoria: async(p) => {
+        await EliminarCategoria(p);
+        const {mostrarCategorias} = get();
+        set(mostrarCategorias(p));
+    },
+    editarCategoria: async(p) => {
+        await EditarCategoria(p);
+        const {mostrarCategorias} = get();
+        set(mostrarCategorias(p));
+    }
+}))
